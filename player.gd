d4cee6c3e49ed00c24834aec7f957e03f82cd6b4
Derek Lanz <143565472+dlanz1@users.noreply.github.com>
2025-09-15 01:13:28 -0700

    Add notebook paper background texture and respawn script + nodes, ink blot video attribution

--- player.gd
+++ player.gd
@@ -6,6 +6,12 @@ const JUMP_VELOCITY = -255.0
 
 @onready var sprite = $AnimatedSprite2D
 
+var spawn_position: Vector2
+
+func _ready() -> void:
+	# set the initial spawn positionbto where the player is first placed in the level
+	spawn_position = global_position
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -23,7 +29,6 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
-	
 	# Sprite direction changing based on direction left/right
 	if direction > 0:
 		sprite.flip_h = false
@@ -31,3 +36,12 @@ func _physics_process(delta: float) -> void:
 		sprite.flip_h = true
 
 	move_and_slide()
+
+func respawn() -> void:
+	print("Player respawning!")
+	global_position = spawn_position
+	velocity = Vector2.ZERO
+
+
+func _on_kill_zone_body_entered(body: Node2D) -> void:
+	respawn()
